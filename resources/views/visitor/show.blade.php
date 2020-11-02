e9fea83ee3e463f45744575286736e603901219f
sudo4 <dedy_setyawann@yahoo.com>
2020-11-02 21:38:10 +0700

    add

--- resources/views/visitor/show.blade.php
+++ resources/views/visitor/show.blade.php
@@ -12,8 +12,8 @@
                     <div class="col-md-7 align-self-center text-right">
                         <div class="d-flex justify-content-end align-items-center">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="javascript:void(0)"></a></li>
-                                <li class="breadcrumb-item active"></li>
+                                <li></li>
+                                <li></li>
                             </ol>
                             
                         </div>
@@ -21,21 +21,55 @@
                 </div>
                  <div class="row justify-content-center">
                     <!-- Column -->
-                    <div class="col-lg-4 col-xlg-3 col-md-5">
-                        <div class="card"> <img class="card-img" src="../assets/images/background/socialbg.jpg" height="456" alt="Card image">
+                    <div class="col-lg-4">
+                        <div class="card"> <img class="card-img"  height="456" alt="Card image">
                             <div class="card-img-overlay card-inverse text-white social-profile d-flex justify-content-center">
                                 <div class="align-self-center"> <img src="../assets/images/users/1.jpg" class="img-circle" width="100">
-                                    <h4 class="card-title">James Anderson</h4>
-                                    <h6 class="card-subtitle">@jamesandre</h6>
+                                    <h4 class="card-title"></h4>
+                                    <h4 class="card-title">{{$visitor->nama}}</h4>
+                                    <h6 class="card-subtitle">{{$visitor->nik}}</h6>
+                                    <h6 class="card-subtitle">{{$visitor->uuid}}</h6>
                                     <div class="text-white">
                                     	<form id="personal-info" action="{{route('visitor.update', $visitor->uuid)}}" method="post">
-                                @csrf
-                                @method('patch')
-                                <button type="submit" href="/visitor" class="btn btn-block btn-outline-warning icheck-material-primary"> 
-                                    <input id="success1" type="radio" name="konfirmasi" value="tidak_hadir" checked="tidak_hadir">
-                                    CANCEL
-                                </button>
-                            </form>
+			                                @csrf
+			                                @method('patch')
+			                                <button type="submit" href="/visitor" class="btn btn-block btn-outline-warning icheck-material-primary"> 
+			                                    <input id="success1" type="radio" name="konfirmasi" value="tidak_hadir" checked="tidak_hadir" style="opacity: 0%">
+			                                    CANCEL
+			                                    <input id="success1" type="radio" name="konfirmasi" style="opacity: 0%"> 
+			                                </button>
+			                            </form>
+			                            <br>
+			                            <form id="personal-info" action="{{route('visitor.update', $visitor->uuid)}}" method="post">
+			                                @csrf
+			                                @method('patch')
+			                                <button type="submit" href="/visitor" class="btn btn-block btn-outline-primary icheck-material-warning"> 
+			                                    <input id="success1" type="radio" name="konfirmasi" value="hadir" checked="hadir" style="opacity: 0%">
+			                                    CONFIRM
+			                                    <input id="success1" type="radio" name="konfirmasi" style="opacity: 0%"> 
+			                                </button>
+			                            </form>
+			                            <br>
+			                            @role('superadministrator')
+					                    
+					         
+					                          <button class="btn btn-block btn-outline-info icheck-material-primary" data-toggle="modal" data-target="#smallsizemodal">QRCODE</button>
+					                          <!-- Modal -->
+					                            <div class="modal fade" id="smallsizemodal">
+					                              <div class="modal-dialog modal-sm">
+					                                <div class="modal-content text-center" style="background: white;">
+					                                  <div class="modal-header">
+					                                    <div class="modal-body">
+					                                    <h3 class="text-secondary">{{$visitor->nama}}</h3>
+					                                    <hr>
+					                                      {!! QrCode::size(220)->generate(Request::url()); !!}
+					                                    </div>
+					                                  </div>
+					                                </div>
+					                              </div>
+					                            </div>
+					                 
+					                    @endrole 
                                     </div>
                                 </div>
                             </div>
