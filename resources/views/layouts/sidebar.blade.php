6e79ee6817721c5f68b55c8cab84f27d1f3839d7
sudo4 <dedy_setyawann@yahoo.com>
2020-11-09 11:30:06 +0700

    button

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -30,15 +30,15 @@
                                 <li><a href="/profile">Delegasi </a></li>
                             </ul>
                         </li>
-
+                        @role('superadministrator')
                         <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-speedometer"></i><span class="hide-menu">Backend</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="/admin/users">User </a></li>
-                                <li><a href="/">Roles </a></li>
-                                <li><a href="/">Permission </a></li>
+                                <li><a href="/roles">Roles </a></li>
+                                <li><a href="/permission">Permission </a></li>
                             </ul>
                         </li>
-
+                        @endrole
 
                         <li> <a class="waves-effect waves-dark" href="/logActivity" aria-expanded="false"><i class="fa fa-circle-o text-danger"></i><span class="hide-menu">Documentation</span></a></li>
                         <li> <a class="waves-effect waves-dark" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fa fa-circle-o"></i> {{ __('Logout') }} </a> <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none"> @csrf </form></li>
